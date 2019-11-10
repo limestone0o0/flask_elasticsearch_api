1ecf95c0b747d61d0f0df67825bb50344d51c324
limestone0o0 <33622673+limestone0o0@users.noreply.github.com>
2019-11-10 17:20:31 +0800

    change privacy

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ STATICFILES_DIR = os.path.join(BASE_DIR, "static")
 class Develop(object):
     DEBUG = True
     SECRET_KEY = "debug"
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:7890@39.107.253.135:33060/myblog?charset=utf8'  # 数据库地址 sqllite
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:7890@127.0.0.1:3306/myblog?charset=utf8'  # 数据库地址 sqllite
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True  # 请求结束后自动提交
     SQLALCHEMY_TRACK_MODIFICATIONS = True  # flask1版本之后，添加的选项，目的是跟踪修改
 
